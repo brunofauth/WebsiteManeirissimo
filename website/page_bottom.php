0023d6681fce790804103e1f960cd4502db0b40a
bruno <149593@upf.br>
2018-11-12 15:10:48 -0200

    ultima mudanca, de verdade!

--- website/page_bottom.php
+++ website/page_bottom.php
@@ -1,8 +1,8 @@
         </main>
         <footer>
-            <p class="footer-item">© 2018 — O Grande Fauth</p>
-            <a class="footer-item">Termos e Condições</a>
-            <a class="footer-item">Sobre</a>
+            <p class="footer-item">© 2018 — Bruno Vinícius Fauth</p>
+            <p class="footer-item">Contato: grande_fauth@ben10fc.com</p>
+            <p class="footer-item">Fone: (11) 99897-3256</p>
         </footer>
     </body>
 </html>
